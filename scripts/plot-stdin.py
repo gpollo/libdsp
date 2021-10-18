846d0e1627de2116ac5ab42bc9b8f3faa8abfa4b
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2021-10-18 13:27:42 -0400

    Make limits optional and add legend in plot script

--- scripts/plot-stdin.py
+++ scripts/plot-stdin.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import fileinput
+import matplotlib
 import matplotlib.pyplot
+matplotlib.rcParams['text.usetex'] = True
 
 settings = dict()
 
@@ -10,19 +12,33 @@ ys = []
 for line in fileinput.input():
     if line[0] == '>':
         values = line[1:].split('=')
-        settings[values[0]] = values[1]
+        settings[values[0]] = '='.join(values[1:])
     else:
         values = line.split(' ')
         values = [float(x) for x in values]
         xs.append(values[0])
         ys.append(values[1:])
 
+show_legend = False
 for i in range(len(ys[0])):
-    matplotlib.pyplot.plot(xs, [t[i] for t in ys])
+    title_key = 'title-{}'.format(i)
+    if title_key in settings:
+        matplotlib.pyplot.plot(xs, [t[i] for t in ys], label=settings[title_key])
+        show_legend = True
+    else:
+        matplotlib.pyplot.plot(xs, [t[i] for t in ys])
+
+if show_legend:
+    matplotlib.pyplot.legend()
 
 matplotlib.pyplot.title(settings['title'])
 matplotlib.pyplot.xlabel(settings['x-axis'])
 matplotlib.pyplot.ylabel(settings['y-axis'])
-matplotlib.pyplot.xlim(float(settings['x-min']), float(settings['x-max']))
-# matplotlib.pyplot.ylim(float(settings['y-min']), float(settings['y-max']))
+
+if 'x-min' in settings and 'x-max' in settings:
+    matplotlib.pyplot.xlim(float(settings['x-min']), float(settings['x-max']))
+
+if 'y-min' in settings and 'y-max' in settings:
+    matplotlib.pyplot.ylim(float(settings['y-min']), float(settings['y-max']))
+
 matplotlib.pyplot.show()
